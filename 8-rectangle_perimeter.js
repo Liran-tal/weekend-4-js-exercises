0766818b9a06c3f88f4c52a081e49e6f662e3975
Liran-tal <taliran44@gmail.com>
2021-11-18 17:35:16 +0200

    8-rectangle_perimeter.js

---
+++ 8-rectangle_perimeter.js
@@ -0,0 +1,7 @@
+function getRectanglePerimeter(width, heigth) {
+	return 2 * (width + heigth);
+}
+
+console.log("getRectanglePerimeter(6, 7): ", getRectanglePerimeter(6, 7));
+console.log("getRectanglePerimeter(20, 10): ", getRectanglePerimeter(20, 10));
+console.log("getRectanglePerimeter(2, 9): ", getRectanglePerimeter(2, 9));
