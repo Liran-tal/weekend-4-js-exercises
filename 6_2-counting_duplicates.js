843d8948cb2202e4166b41686b9cd600d780e511
Liran-tal <taliran44@gmail.com>
2021-11-18 15:13:29 +0200

    6_2-counting_duplicates.js

---
+++ 6_2-counting_duplicates.js
@@ -0,0 +1,26 @@
+function countingDuplicates(arg_str) {
+	
+	let counter = 0;
+	let dup_arr = [];
+	const tally = {};
+	let loc_str = arg_str.toLowerCase().split('');
+
+	for (char of loc_str){
+		char in tally 
+			? ++ tally[char] 
+			: tally[char] = 1;
+	}
+	return (
+		Object.values(tally).reduce((counter, value) => {
+			return value > 1 ? ++ counter : counter;
+		}, 0)
+	);
+}
+
+console.log("countingDuplicates('abCde'): ", countingDuplicates('abCde'));
+console.log("countingDuplicates('aabbCde'): ", countingDuplicates('aabbCde'));
+console.log("countingDuplicates('aabBCde'): ", countingDuplicates('aabBCde'));
+console.log("countingDuplicates('invisibility'): ", countingDuplicates('invisibility'));
+console.log("countingDuplicates('invisibilities'): ", countingDuplicates('invisibilities'));
+console.log("countingDuplicates('aA11'): ", countingDuplicates('aA11'));
+console.log("countingDuplicates('ABBA'): ", countingDuplicates('ABBA'));
