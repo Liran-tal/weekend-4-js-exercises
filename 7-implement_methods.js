0d739f8f78ca2ad19522d47f865b4b04019348aa
Liran-tal <taliran44@gmail.com>
2021-11-18 21:48:04 +0200

    7-implement_methods.js - map

--- 7-implement_methods.js
+++ 7-implement_methods.js
@@ -1,9 +1,14 @@
 const test_array = ['a', 3, 2, 'd', 'S', 1, 5, 't', 'f',];
 
-function myFilter(array, callbackFn) {
+function myFilter(array, callbackFn, thisArg) {
 	const res_arr = [];
 	for (let index = 0; index < array.length; index++) {
-		let res = callbackFn(array[index], index, array);
+		let res = undefined;
+		if (!thisArg) {
+			res = callbackFn(array[index], index, array);
+		} else{
+			res = callbackFn(array[index], index, array, thisArg);
+		}
 		if (res == true){
 			res_arr.push(array[index]);
 		}
@@ -19,29 +24,60 @@ function filterNumbers(input_array) {
 	return myFilter(input_array, callbackFn);
 }
 
-console.log(filterNumbers(test_array));
+// console.log(filterNumbers(test_array));
 
 
 // ********************
 
 
-function myFilter(array, callbackFn) {
+function myForEach(array, callbackFn, thisArg) {
+	for (let index = 0; index < array.length; index++) {
+		if (!thisArg) {
+			callbackFn(array[index], index, array);
+		} else{
+			callbackFn(array[index], index, array, thisArg);
+		}
+	} 
+
+	return undefined;
+}
+
+function printArray(input_array) {
+	const callbackFn = (element) => {
+		console.log(element);
+	}
+	myForEach(input_array, callbackFn);
+	return 0;
+}
+
+// printArray(test_array);
+
+
+//**************************** */
+
+
+function myMap(array, callbackFn, thisArg) {
 	const res_arr = [];
 	for (let index = 0; index < array.length; index++) {
-		let res = callbackFn(array[index], index, array);
-		if (res == true){
-			res_arr.push(array[index]);
+		let res = undefined;
+		if (!thisArg) {
+			res = callbackFn(array[index], index, array);
+		} else{
+			res = callbackFn(array[index], index, array, thisArg);
 		}
+		console.log(res);
+		res_arr.push(res);
 	} 
 
 	return res_arr;
 }
 
-function filterNumbers(input_array) {
+function toCharsArray(input_array) {
 	const callbackFn = (element) => {
-		return isNaN(element) == false; 
+		console.log(element);
+		return element.toString(); 
 	}
-	return myFilter(input_array, callbackFn);
+	return myMap(input_array, callbackFn);
 }
 
-console.log(filterNumbers(test_array));
+console.log(toCharsArray(test_array));
