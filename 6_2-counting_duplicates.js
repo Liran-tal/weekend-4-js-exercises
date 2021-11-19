41e4dde403b2a78724109a7e289e4f52c5826c65
Liran-tal <taliran44@gmail.com>
2021-11-19 08:40:29 +0200

    6_2-counting_duplicates.js with second option

--- 6_2-counting_duplicates.js
+++ 6_2-counting_duplicates.js
@@ -1,23 +1,34 @@
 function countingDuplicates(arg_str) {
 	const tally = {};
+	let res = {};
 	let loc_str = arg_str.toLowerCase().split('');
 
 	for (char of loc_str){
 		char in tally 
 			? ++ tally[char] 
 			: tally[char] = 1;
+		}
+	// Exercise option: return count of letters that repeats
+	// return (
+	// 	Object.values(tally).reduce((counter, value) => {
+	// 		return value > 1 ? ++ counter : counter;
+	// 	}, 0)
+	// );
+
+	// Exercise option: return the letters that repeats and repeat count for each
+	for (const key in tally) {
+		if (tally[key] > 1) {
+			res[key] = tally[key];
+		}
 	}
-	return (
-		Object.values(tally).reduce((counter, value) => {
-			return value > 1 ? ++ counter : counter;
-		}, 0)
-	);
+	
+	return res;
 }
 
 console.log("countingDuplicates('abCde'): ", countingDuplicates('abCde'));
 console.log("countingDuplicates('aabbCde'): ", countingDuplicates('aabbCde'));
 console.log("countingDuplicates('aabBCde'): ", countingDuplicates('aabBCde'));
-console.log("countingDuplicates('invisibility'): ", countingDuplicates('invisibility'));
-console.log("countingDuplicates('invisibilities'): ", countingDuplicates('invisibilities'));
+console.log("countingDuplicates('indivisibility'): ", countingDuplicates('indivisibility'));
+console.log("countingDuplicates('indivisibilities'): ", countingDuplicates('indivisibilities'));
 console.log("countingDuplicates('aA11'): ", countingDuplicates('aA11'));
 console.log("countingDuplicates('ABBA'): ", countingDuplicates('ABBA'));
